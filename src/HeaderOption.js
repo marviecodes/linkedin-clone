b8ac7a14bbbd598ecac0069a2c0ee005d2c96d2e
marviecodes <marvnonso554@gmail.com>
2024-03-27 10:46:30 +0100

    Updated README.md

--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -2,7 +2,7 @@ import "./HeaderOption.css";
 
 const HeaderOption = ({ title, Icon }) => {
   return (
-    <div className="HeaderOption">
+    <div className="headerOption">
       {Icon && <Icon className="HeaderOption__icon" />}
       <h3 className="HeaderOption__title">{title}</h3>
     </div>
